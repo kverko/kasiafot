2d7d12069658e99985c6344eb96bd872d8ffb899
Sławomir Rubiszewski <slawomirr@gmail.com>
2016-01-17 01:14:27 +0100

    trying forms

--- main.go
+++ main.go
@@ -3,20 +3,31 @@ package main
 
 import (
 	"fmt"
+	"html/template"
 	"net/http"
 )
 
 func index(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, "hi, this is index")
+	t, _ := template.ParseFiles("templates/index.html")
+	t.Execute(w, nil)
 }
 
 func admin(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, "hi, this is admin")
+	if r.Method == "POST" {
+		r.ParseForm()
+		fmt.Println("user: ", r.Form["user"])
+		fmt.Println("pass: ", r.Form["pass"])
+	} else {
+		t, _ := template.ParseFiles("templates/admin.html")
+		t.Execute(w, nil)
+	}
 }
 
 func main() {
 	http.HandleFunc("/", index)
 	http.HandleFunc("/admin", admin)
-
-	http.ListenAndServe(":8000", nil)
+	err := http.ListenAndServe(":8000", nil)
+	if err != nil {
+		log.Fatal("ListenAndServe", err)
+	}
 }
