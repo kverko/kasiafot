8d4b4229272d081a3b84448c54c5c2e10d7caf53
Sławomir Rubiszewski <slawomirr@gmail.com>
2016-01-19 00:46:14 +0100

    simple upload form

--- main.go
+++ main.go
@@ -2,9 +2,15 @@
 package main
 
 import (
+	"crypto/md5"
 	"fmt"
 	"html/template"
+	"io"
+	"log"
 	"net/http"
+	"os"
+	"strconv"
+	"time"
 )
 
 func index(w http.ResponseWriter, r *http.Request) {
@@ -15,17 +21,69 @@ func index(w http.ResponseWriter, r *http.Request) {
 func admin(w http.ResponseWriter, r *http.Request) {
 	if r.Method == "POST" {
 		r.ParseForm()
+		token := r.Form.Get("token")
+		if token != "" {
+			if senttok == token {
+				fmt.Println("the same token")
+			} else {
+				senttok = token
+			}
+		} else {
+
+		}
 		fmt.Println("user: ", r.Form["user"])
 		fmt.Println("pass: ", r.Form["pass"])
 	} else {
+		crutime := time.Now().Unix()
+		h := md5.New()
+		io.WriteString(h, strconv.FormatInt(crutime, 10))
+		token := fmt.Sprintf("%x", h.Sum(nil))
 		t, _ := template.ParseFiles("templates/admin.html")
-		t.Execute(w, nil)
+		t.Execute(w, token)
 	}
 }
 
+func upload(w http.ResponseWriter, r *http.Request) {
+	if r.Method == "GET" {
+		crutime := time.Now().Unix()
+		h := md5.New()
+		io.WriteString(h, strconv.FormatInt(crutime, 10))
+		token := fmt.Sprintf("%x", h.Sum(nil))
+		t, _ := template.ParseFiles("templates/upload.html")
+		t.Execute(w, token)
+	} else {
+		r.ParseMultipartForm(32 << 20)
+		file, handler, err := r.FormFile("file")
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+		defer file.Close()
+		fmt.Fprintf(w, "%v", handler.Header)
+		dir_path := "./test/"
+		err = os.MkdirAll(dir_path, os.ModePerm)
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+		f, err := os.OpenFile(dir_path+handler.Filename, os.O_WRONLY|os.O_CREATE, 0666)
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+		defer f.Close()
+		io.Copy(f, file)
+	}
+
+}
+
+var senttok string
+
 func main() {
+	senttok = ""
 	http.HandleFunc("/", index)
 	http.HandleFunc("/admin", admin)
+	http.HandleFunc("/upload", upload)
 	err := http.ListenAndServe(":8000", nil)
 	if err != nil {
 		log.Fatal("ListenAndServe", err)
