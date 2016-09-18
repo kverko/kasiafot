5f5d1f3d82f4784b05d1ce99841134d486920898
rubi <slawomirr@gmail.com>
2016-09-18 11:53:46 +0200

    making order in session comment

--- sessions/session.go
+++ sessions/session.go
@@ -1,7 +1,6 @@
 package sessions
 
-//session itself: structure and functionality
-
+//Session - structure for keeping session id and related values
 type Session struct {
 	id     string
 	values map[string]interface{}
